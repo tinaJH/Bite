6204710f1fa55529fae520151a8d66afe3c8229b
tinaJH <21jxiong@gmail.com>
2017-08-26 16:10:36 -0500

    Initial Commit

---
+++ SummerYelpMock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SummerYelpMock
+//
+//  Created by Jinhua Xiong on 8/26/17.
+//  Copyright © 2017 Jinhua Xiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SummerYelpMock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SummerYelpMock
+//
+//  Created by Jinhua Xiong on 8/26/17.
+//  Copyright © 2017 Jinhua Xiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
